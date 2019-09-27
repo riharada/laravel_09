5c32f0207164a8a825bb9673d84af09b90b40b30
haradariho <tawawan218@gmail.com>
2019-09-27 09:51:09 +0000

    Laravel_10の更新

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>MyHobby</title>
+  </head>
+  <body>
+    <h1>趣味</h1>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix =>'admin'], function(){
-  Route::get('news/create','Admin\NewsController@add');
-});
-
 //課題3
 // Route::get('admin/news/create',XXX\AAAController@add');
 
-Route::group(['prefix =>'admin'], function(){
+Route::group(['prefix' =>'admin'], function() {
+  Route::get('news/create','Admin\NewsController@add');
   Route::get('profile/create','Admin\ProfileController@add');
   Route::get('profile/edit','Admin\ProfileController@add');
 });
